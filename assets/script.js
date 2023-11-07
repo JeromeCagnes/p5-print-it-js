62c9683972815e3614c884ebdd8555fb9c093ebc
JeromeCagnes <136485880+JeromeCagnes@users.noreply.github.com>
2023-11-07 09:39:54 +0100

    suivi et décrémentation de l'index, Maj jour du bullet point actif, retour diapo 1

--- assets/script.js
+++ assets/script.js
@@ -24,18 +24,65 @@ const arrowRight = document.querySelector('.arrow_right')
 
 /* Étape 3 : Ajoutez des bullet points au slider */
 
+const numberOfSlides = slides.length // compter elements du tableau
 const dotsContainer = document.querySelector('.dots')
 
-// Générer les bullet points
-slides.forEach((slide, index) => {
-  const dot = document.createElement('div')
-  dot.classList.add('dot')
-  if (index === 0) {
-    // Le premier bullet est actif par défaut
-    dot.classList.add('active')
+for (let i = 0; i < numberOfSlides; i++) {
+  const dot = document.createElement('span') //création de l'élément <span>
+  dot.classList.add('dot') //Ajout classe 'dot' à chaque point
+  dotsContainer.appendChild(dot) //Ajout point à l'element <div class="dots">
+}
+const firstDot = dotsContainer.querySelector('dot') //Ajout classe "dot_selected" au premier point
+if (firstDot) {
+  firstDot.classList.add('dot_selected') //Styliser le premier bullet point actif
+}
+
+//ETAPE 4
+let currentSlide = 0 // Variable pour suivre l'index de la diapositive actuelle
+
+arrowLeft.addEventListener('click', function () {
+  console.log('Clic flèche gauche')
+  currentSlide-- // Décrémenter l'index de la diapositive actuelle
+
+  if (currentSlide < 0) {
+    // Vérifier si index est inférieur à zéro pour revenir à la dernière diapositive ETAPE 5
+    currentSlide = numberOfSlides - 1
   }
-  dot.addEventListener('click', () => {
-    // Changer de diapositive lorsque l'on clique sur un bullet point
+
+  const slide = slides[currentSlide] // Mise à jour image et texte correspondants à la diapo actuelle
+  const image = document.querySelector('.banner-img')
+  const tagLine = document.querySelector('#banner p')
+  image.src = './assets/images/slideshow/' + slide.image
+  tagLine.innerHTML = slide.tagLine
+
+  const dots = dotsContainer.querySelectorAll('.dot') // Mise à jour du bullet point actif
+  dots.forEach((dot, index) => {
+    dot.classList.remove('dot_selected')
+    if (index === currentSlide) {
+      dot.classList.add('dot_selected')
+    }
   })
-  dotsContainer.appendChild(dot)
+})
+//ETAPE 5
+
+arrowRight.addEventListener('click', function () {
+  console.log('Clic flèche droite')
+  currentSlide++ // Incrémenter index de la diapo actuelle
+  if (currentSlide >= numberOfSlides) {
+    // Verifier si index est sup ou = au nombre de diapos pour revenir à la 1ere diapositive
+    currentSlide = 0
+  }
+  const slide = slides[currentSlide] // Mise à jour image et  texte correspondants à la diapo en cours
+  const image = document.querySelector('.banner-img')
+  const tagLine = document.querySelector('#banner p')
+  image.src = './assets/images/slideshow/' + slide.image
+  tagLine.innerHTML = slide.tagLine
+
+  const dots = dotsContainer.querySelectorAll('.dot') // Mise à jour du bullet point actif
+  dots.forEach((dot, index) => {
+    dot.classList.remove('dot_selected')
+    if (index === currentSlide) {
+      dot.classList.add('dot_selected')
+    }
+  })
 })
